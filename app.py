fd43478efe5fcd0b0b4d8ff709ea093df7ef7940
shaked123456 <97021896+shaked123456@users.noreply.github.com>
2022-01-06 17:56:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Test :P"
-
-main():
-    app.run(debug=True, host="0.0.0.0", port="2222")
     
 if __name__ == "__main__":
-    main()
+    app.run(debug=True, host="0.0.0.0", port="2222")
